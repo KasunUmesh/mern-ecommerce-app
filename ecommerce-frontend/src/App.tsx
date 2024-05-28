e0bf050a9e5826343d946b625dc462a735685de2
Kasun Umesh <kasunumesh38@gmail.com>
2024-05-29 00:29:38 +0530

    init project

---
+++ ecommerce-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl text-red-500 border border-blue-500'>Hello Welcome</h1>
+    </>
+  )
+}
+
+export default App
