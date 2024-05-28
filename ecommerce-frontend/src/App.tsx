e8575f0c5148579177247604a29e9402c2237a7d
Kasun Umesh <kasunumesh38@gmail.com>
2024-05-29 00:46:48 +0530

    add assets images

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,13 +1,12 @@
-
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
-      <h1 className='text-3xl text-red-500 border border-blue-500'>Hello Welcome</h1>
-    </>
+    <main>
+      
+    </main>
   )
 }
 
