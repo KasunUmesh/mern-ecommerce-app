86a4c73e251e180ee98df77fc2b3bbd78dd45b5e
Kasun Umesh <kasunumesh38@gmail.com>
2024-05-29 03:05:20 +0530

    routers created

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,11 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/layout/Header'
+import Home from './pages/Home'
+import Category from './pages/Category'
+import Product from './pages/Product'
+import Footer from './components/layout/Footer'
 
 function App() {
   
 
   return (
-    <main>
-      
+    <main className='text-[#222222]'>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/clothing' element={<Category/>}/>
+          <Route path='/cosmetics' element={<Category/>}/>
+          <Route path='/electronics' element={<Category/>}/>
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </main>
   )
 }
