e06a4f1050b77677aca6be527207e16a71247b54
Kasun Umesh <kasunumesh38@gmail.com>
2024-06-11 02:36:30 +0530

    product category created

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -5,6 +5,9 @@ import Home from './pages/Home'
 import Category from './pages/Category'
 import Product from './pages/Product'
 import Footer from './components/layout/Footer'
+import clothingbanner from './assets/clothingbanner.png'
+import cosmeticsbanner from './assets/cosmeticsbanner.png'
+import electronicsbanner from './assets/electronicsbanner.png'
 
 function App() {
   
@@ -15,9 +18,9 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/clothing' element={<Category/>}/>
-          <Route path='/cosmetics' element={<Category/>}/>
-          <Route path='/electronics' element={<Category/>}/>
+          <Route path='/clothing' element={<Category category={"clothing"} banner={clothingbanner}/>}/>
+          <Route path='/cosmetics' element={<Category category={"cosmetics"} banner={cosmeticsbanner}/>}/>
+          <Route path='/electronics' element={<Category category={"electronics"} banner={electronicsbanner}/>}/>
           <Route path='/product' element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           </Route>
--- ecommerce-frontend/src/pages/Category.tsx
+++ ecommerce-frontend/src/pages/Category.tsx
@@ -1,8 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
+import { VscSettings  } from "react-icons/vsc";
+import all_products from '../utils/all_products';
+import Item from '../components/card/Item';
+import { Link } from 'react-router-dom';
 
-const Category = () => {
+const Category = ({banner, category}: any) => {
   return (
-    <div>Category</div>
+    <section className='mx-auto max-w-full px-6 lg:px-20 bg-[#f8f7f4]'>
+      <div>
+        <div className='pt-6'>
+          <img src={banner} alt="" className='block mb-7 mx-auto'/>
+        </div>
+        <div className='flex items-center justify-between my-10 mx-2'>
+          <h5><span className='font-bold'>Showing 1-12</span> out of 36 products</h5>
+          <Link to={'/'}><VscSettings  className='text-3xl bg-[#222222] rounded-md h-10 w-10 p-2 text-white'/></Link>
+        </div>
+        {/* container */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 gap-y-28 mt-32'>
+          {all_products.map((item) => {
+            if (category === item.category) {
+            return <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            }
+          })}      
+        </div>
+        <div className='py-16 text-center'>
+          <button className='ring-1 ring-[#222222] text-[15px] font-[500] bg-[#222222] px-7 py-[10px] text-white transition-all hover:bg-[#222222]/90 rounded-xl'>Load more</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
