f84913fb1e0390b7bd6b36da47eab45bb7f2a305
Kasun Umesh <kasunumesh38@gmail.com>
2024-06-29 06:03:55 +0530

    backend config and image upload function creating

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,54 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import multer from "multer";
+import cors from "cors";
+import path from "path";
 
+
+const port = 4000;
 const app = express();
 
+app.use(express.json());
+app.use(cors());
 
+mongoose.connect(process.env.MONGO_URL as string)
+const db = mongoose.connection
 
-app.get("/", (req, res) => {
+db.on('error', (error) => {
+    console.log("DB Connection Error: ", error);
+})
+
+db.on('open', () => {
+    console.log("DB Connected");
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port: ", port);
+})
+
+app.get('/', (req, res) => {
     res.send("Hello World!");
 })
 
-mongoose.connect("mongodb://127.0.0.1:27017/ijse-ecommerce").then(() => {
-    console.log("database connect success")
-    app.listen(3000, () => {
-        console.log(`Server started on port ${3000}`);
-    })
-  }).catch((err) => {
-    console.log("can not connect database");
-    console.log(err);
-});
+// Image storage engine
+const storage = multer.diskStorage({
+    destination: 'src/upload/images',
+    filename:(req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
 
-// app.listen(3000, () => {
-//     console.log(`Server started on port ${3000}`);
-// })
+const upload = multer({storage: storage});
+
+// Creating endpoint for images
+app.use('images', express.static('src/upload/images'))
+app.post("/upload", upload.single('product'), (req: express.Request, res: express.Response) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file?.filename}`
+    })
+})
