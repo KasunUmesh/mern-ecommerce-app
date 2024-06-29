5b0ba95adb176d496ec076b90291cd8dc4f9401e
Kasun Umesh <kasunumesh38@gmail.com>
2024-06-29 23:16:02 +0530

    AllProduct get function create

--- admin/src/pages/AddProduct.tsx
+++ admin/src/pages/AddProduct.tsx
@@ -3,9 +3,17 @@
 import Navbar from "../components/layout/navbar";
 import { Pen, Trash2 } from "lucide-react";
 import upload_area from "../assets/upload_area.svg"
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import axios from "axios";
 
+
+interface Data {
+  name: string;
+  image: string;
+  category: string;
+  new_price: string;
+  old_price: string;
+}
 
 
 export default function AddProduct() {
@@ -16,7 +24,8 @@ export default function AddProduct() {
     category: "clothing",
     new_price: "",
     old_price: "",
-  })
+  });
+  const [allProducts, setAllProducts] = useState<Data[]>([]);
 
   const changeHandle = (e: any) => {
     setProductDetails({...productDetails, [e.target.name]: e.target.value})
@@ -68,6 +77,18 @@ export default function AddProduct() {
     }
   }
 
+  const fetchInfo = async () => {
+    await axios.get('http://localhost:4000/allproducts').then(Response => {
+      setAllProducts(Response.data);
+    }).catch(error => {
+      alert(error);
+    })
+  }
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
 
   return (
     <div className="bg-[#F5F8FE] h-screen w-full">
@@ -80,20 +101,53 @@ export default function AddProduct() {
               <div className="mt-6 gap-5 md:grid-cols-2 !z-5 rounded-[20px] bg-white bg-clip-border shadow-3xl shadow-shadow-500 shadow-sm">
                 <div className="p-5 flex gap-5 justify-center">
                   <div>
-                    <h4 className="block mb-1 text-sm text-gray-500 font-medium">Product Title</h4>
-                    <input value={productDetails.name} onChange={changeHandle} type="text" name="name" placeholder="Enter Title" className="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-purple-500 block w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white"/>
+                    <h4 className="block mb-1 text-sm text-gray-500 font-medium">
+                      Product Title
+                    </h4>
+                    <input
+                      value={productDetails.name}
+                      onChange={changeHandle}
+                      type="text"
+                      name="name"
+                      placeholder="Enter Title"
+                      className="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-purple-500 block w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white"
+                    />
                   </div>
                   <div className="w-96">
-                  <h4 className="block mb-1 text-sm text-gray-500 font-medium">Price</h4>
-                  <input value={productDetails.old_price} onChange={changeHandle} type="text" name="old_price" placeholder="Enter Price" className="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-purple-500 block w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white"/>
+                    <h4 className="block mb-1 text-sm text-gray-500 font-medium">
+                      Price
+                    </h4>
+                    <input
+                      value={productDetails.old_price}
+                      onChange={changeHandle}
+                      type="text"
+                      name="old_price"
+                      placeholder="Enter Price"
+                      className="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-purple-500 block w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white"
+                    />
                   </div>
                   <div className="w-96">
-                  <h4 className="block mb-1 text-sm text-gray-500 font-medium">Offer Price</h4>
-                  <input value={productDetails.new_price} onChange={changeHandle} type="text" name="new_price" placeholder="Enter Offer Price" className="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-purple-500 block w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white"/>
+                    <h4 className="block mb-1 text-sm text-gray-500 font-medium">
+                      Offer Price
+                    </h4>
+                    <input
+                      value={productDetails.new_price}
+                      onChange={changeHandle}
+                      type="text"
+                      name="new_price"
+                      placeholder="Enter Offer Price"
+                      className="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-purple-500 block w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white"
+                    />
                   </div>
                   <div className="w-72">
-                    <h4 className="block mb-1 text-sm font-medium text-gray-500">Product Category</h4>
-                    <select name="category" id="" className="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-purple-500 block w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white">
+                    <h4 className="block mb-1 text-sm font-medium text-gray-500">
+                      Product Category
+                    </h4>
+                    <select
+                      name="category"
+                      id=""
+                      className="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-purple-500 block w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white"
+                    >
                       <option value="clothing">Clothing</option>
                       <option value="cosmetics">Cosmetics</option>
                       <option value="electronics">Electronics</option>
@@ -101,9 +155,20 @@ export default function AddProduct() {
                   </div>
                   <div className="">
                     <label htmlFor="file-input">
-                      <img src={image ? URL.createObjectURL(image) : upload_area} alt="" className="w-20 rounded-sm inline-block"/>
+                      <img
+                        src={image ? URL.createObjectURL(image) : upload_area}
+                        alt=""
+                        className="w-20 rounded-sm inline-block"
+                      />
                     </label>
-                    <input onChange={imagehandler} type="file" name="image" id="file-input" hidden className="bg-[#f8f7f4] max-w-80 w-full py-3 px-4" />
+                    <input
+                      onChange={imagehandler}
+                      type="file"
+                      name="image"
+                      id="file-input"
+                      hidden
+                      className="bg-[#f8f7f4] max-w-80 w-full py-3 px-4"
+                    />
                   </div>
                 </div>
                 <div className="pb-5 flex justify-end pr-36">
@@ -118,21 +183,24 @@ export default function AddProduct() {
               </div>
 
               <div className="mt-7 gap-5 md:grid-cols-2 !z-5 rounded-sm bg-white bg-clip-border shadow-3xl shadow-shadow-500 shadow-sm">
-                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <div className="relative max-h-[60vh] overflow-auto shadow-md sm:rounded-lg">
                   <table className="w-full text-sm text-left rtl:text-right text-blue-100">
                     <thead className="text-xs text-white uppercase bg-indigo-500 text-center">
                       <tr>
                         <th scope="col" className="px-6 py-3">
-                          School ID
+                          PRODUCTS
                         </th>
                         <th scope="col" className="px-6 py-3">
-                          School Full Name
+                          TITLE
                         </th>
                         <th scope="col" className="px-6 py-3">
-                          School Short Name
+                          OLD PRICE
                         </th>
                         <th scope="col" className="px-6 py-3">
-                          Boys / Girls or Mix
+                          NEW PRICE
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                          CATEGORY
                         </th>
                         <th scope="col" className="px-6 py-3">
                           Action
@@ -140,10 +208,43 @@ export default function AddProduct() {
                       </tr>
                     </thead>
                     <tbody>
-                      <tr className=" border-b border-indigo-200 hover:bg-indigo-100 text-black text-center text-base">
+                      {allProducts.map((product, i) => (
+                        <tr
+                          key={i}
+                          className=" border-b border-indigo-200 hover:bg-indigo-100 text-black text-center text-base"
+                        >
+                          <td className="px-6 py-4 flex justify-center">
+                            <img src={product.image} alt="" width={55} height={55}/>
+                          </td>
+                          <td>
+                            <div className="line-clamp-3">{product.name}</div>
+                          </td>
+                          <td>${product.old_price}.00</td>
+                          <td>${product.new_price}.00</td>
+                          <td>{product.category}</td>
+                          <td className="px-6 py-4">
+                            {/* <button
+                              className={
+                                "bg-green-600 text-white p-1 rounded-full mx-2 hover:bg-green-700 hover:scale-[110%] transition duration-300 ease-out"
+                              }
+                            >
+                              <Pen className="p-1" />
+                            </button> */}
+                            <button
+                              className={
+                                "bg-red-600 text-white p-1 rounded-full mx-2 hover:bg-red-700 hover:scale-[110%] transition duration-300 ease-out"
+                              }
+                            >
+                              <Trash2 className="p-1" />
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                      {/* <tr className=" border-b border-indigo-200 hover:bg-indigo-100 text-black text-center text-base">
                         <td className="px-6 py-4">S001</td>
                         <td className="px-6 py-4">Panadura Collage</td>
                         <td className="px-6 py-4">PC</td>
+                        <td className="px-6 py-4">MIX</td>
                         <td className="px-6 py-4">MIX</td>
                         <td className="px-6 py-4">
                           <button
@@ -161,7 +262,7 @@ export default function AddProduct() {
                             <Trash2 className="p-1"/>
                           </button>
                         </td>
-                      </tr>
+                      </tr> */}
                     </tbody>
                   </table>
                 </div>
